b6042cc4d5c2acd80edc2a792b4c850b4d644739
unknown <A183956@PCS48046.grupo.cm.es>
2017-09-12 16:27:12 +0200

    Modificado el main de la aplicacion.

---
+++ src/main/java/EnListame/enlistame_back/Application.java
@@ -0,0 +1,12 @@
+package EnListame.enlistame_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+	
+	public static void main( String[] args ) throws Exception {
+    	SpringApplication.run(Application.class ,args);
+    }
+}
